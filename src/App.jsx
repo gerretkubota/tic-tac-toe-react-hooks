7274632d8c3c519cd16afb109ac2e00feb7538dc
gerretkubota <gerretkubota@gmail.com>
2019-09-07 18:39:53 -0700

    Adding Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './components/Board.jsx';
+import Header from './components/Header.jsx';
 
 import '../public/styles/main.css';
 
 const App = () => (
   <div className="app column column-center">
+    <Header title="Tic Tac Toe" />
     <Board />
   </div>
 );
